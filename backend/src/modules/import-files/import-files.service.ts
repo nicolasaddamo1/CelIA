0b27fc68c7058e05f025a4f409f27da954eb4024
Nico <nicolasaddamo1@gmail.com>
2025-06-09 10:48:09 -0300

    feat: added new dependencies and changes on import file service

--- backend/src/modules/import-files/import-files.service.ts
+++ backend/src/modules/import-files/import-files.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as csv from 'fast-csv';
 
 @Injectable()
-export class ImportFilesService {}
+export class ImportFilesService {
+
+    constructor() { }
+
+    async parseCSV(file: Express.Multer.File) {
+        const transactions: Record<string, any>[] = [];
+        return new Promise((resolve, reject) => {
+            csv.parseString(file.buffer.toString(), { headers: true })
+                .on('data', (row) => transactions.push(row))
+                .on('end', () => resolve(transactions))
+                .on('error', reject);
+        });
+    }
+
+}
