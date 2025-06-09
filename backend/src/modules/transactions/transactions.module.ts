32d49ef88222646fc785b6be1b6c4c6e454a3e5f
Nico <nicolasaddamo1@gmail.com>
2025-06-09 10:39:30 -0300

    feat: added service of transactions

--- backend/src/modules/transactions/transactions.module.ts
+++ backend/src/modules/transactions/transactions.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
+import { TransactionsService } from './transactions.service';
 
 @Module({
-  controllers: [TransactionsController]
+  controllers: [TransactionsController],
+  providers: [TransactionsService]
 })
 export class TransactionsModule {}
