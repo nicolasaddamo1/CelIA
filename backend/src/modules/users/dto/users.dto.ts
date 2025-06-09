e371dc6c303b9329d5fcf1dbbb8f744c9ec18dfd
Nico <nicolasaddamo1@gmail.com>
2025-06-09 15:54:53 -0300

    feat: added updateUserDTO

--- backend/src/modules/users/dto/users.dto.ts
+++ backend/src/modules/users/dto/users.dto.ts
@@ -1,10 +1,13 @@
+import { PartialType } from "@nestjs/mapped-types";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { Transaction } from "src/Entity/transaction.enetity";
 
 export class createUserDto {
+
     @IsString()
     @IsNotEmpty()
     email: string;
+
     @IsString()
     @IsNotEmpty()
     name: string;
@@ -12,3 +15,5 @@ export class createUserDto {
     @IsOptional()
     transactions?: Transaction[];
 }
+
+export class UpdateUserDto extends PartialType(createUserDto) { } 
