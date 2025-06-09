7ef70b95fc926978b9aca59c7c9fe553e742e415
Nico <nicolasaddamo1@gmail.com>
2025-06-09 10:40:11 -0300

    feat: added categories service

--- backend/src/modules/categories/categories.module.ts
+++ backend/src/modules/categories/categories.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 
-@Module({})
+@Module({
+  providers: [CategoriesService]
+})
 export class CategoriesModule {}
