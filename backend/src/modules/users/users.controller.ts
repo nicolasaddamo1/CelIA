169af29f6f4b45476f60e8443ab9c8b1115b260e
Nico <nicolasaddamo1@gmail.com>
2025-06-11 20:49:10 -0300

    feat: added guards

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { createUserDto } from './dto/users.dto';
 import { User } from 'src/Entity/user.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -10,10 +11,12 @@ export class UsersController {
     ) { }
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     async getAllUsers() {
         return await this.usersService.getAllUsers();
     }
     @Get(':id')
+    @UseGuards(JwtAuthGuard)
     async getUsersById(@Param('id') id: string) {
         return await this.usersService.getUsersById(id);
     }
@@ -24,11 +27,13 @@ export class UsersController {
     }
 
     @Put(':id')
+    @UseGuards(JwtAuthGuard)
     async updateUser(@Param('id') id: string, @Body() user: User): Promise<Partial<User> | string> {
         return await this.usersService.updateUser(id, user);
     }
 
     @Delete(':id')
+    @UseGuards(JwtAuthGuard)
     async deleteUser(@Param('id') id: string) {
         return await this.usersService.deleteUser(id);
     }
