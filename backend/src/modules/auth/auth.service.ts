7972c4a8743bf7a3cf0cdfd6f586750462138cb8
Nico <nicolasaddamo1@gmail.com>
2025-06-11 19:46:26 -0300

    feat: added auth

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/auth.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly userService: UsersService,
+    ) { }
+    async signin(dto: LoginDto) {
+        const user = await this.userService.findByEmailWithPassword(dto.email);
+
+        if (typeof user === 'string') {
+            return user;
+        }
+        const isMatch = await bcrypt.compare(dto.password, user.password);
+        if (!isMatch) {
+            throw new UnauthorizedException('Credenciales inválidas');
+        }
+        return this.signToken(user.id, user.email);
+    }
+
+    private signToken(userId: string, email: string) {
+        const payload = { sub: userId, email };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
