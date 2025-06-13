9eccbc9d3ea1af7052839ac45c97a145eb2dc57d
Nico <nicolasaddamo1@gmail.com>
2025-06-13 15:33:14 -0300

    feat: lil fixes on transaction module

--- backend/src/modules/transactions/transactions.module.ts
+++ backend/src/modules/transactions/transactions.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Transaction } from 'typeorm';
 import { UsersModule } from '../users/users.module';
+import { Transaction } from 'src/Entity/transaction.enetity';
+import { UsersService } from '../users/users.service';
+import { User } from 'src/Entity/user.entity';
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([Transaction]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Transaction, User])],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [TransactionsService, UsersService],
   exports: [TransactionsService]
 
 })
