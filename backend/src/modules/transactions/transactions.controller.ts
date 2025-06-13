681aa4e184600a74bcaa9e9a8784e17a0fed7cb3
Nico <nicolasaddamo1@gmail.com>
2025-06-13 17:01:28 -0300

    feat: added new features

--- backend/src/modules/transactions/transactions.controller.ts
+++ backend/src/modules/transactions/transactions.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { CreateTransactionsDto } from './dto/transactions.dto';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+    constructor(private readonly transactionsService: TransactionsService) { }
+
+    @Post()
+    async create(@Body() dto: CreateTransactionsDto, @Req() req: any) {
+        const userId = req.user?.userId;
+        return this.transactionsService.create(userId, dto);
+    }
+
+    @Get()
+    async findAll(@Req() req: any) {
+        const userId = req.user?.userId;
+        return this.transactionsService.findAllByUser(userId);
+    }
+}
